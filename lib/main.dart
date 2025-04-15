e46f7534e7aa8fc0c8014087f5170ad47539c2ec
Melksedeque <freelancer@melksedeque.com.br>
2025-04-15 20:49:17 -0300

    Criação de provider para Restaurant com async e await

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/data/restaurants_data.dart';
+import 'package:myapp/ui/_core/app_theme.dart';
 import 'package:myapp/ui/splash/splash_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  RestaurantsData restaurantData = RestaurantsData();
+  await restaurantData.getRestaurants();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            return restaurantData;
+          },
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,8 +27,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreen()
-    );
+    return MaterialApp(theme: AppTheme.appTheme, home: SplashScreen());
   }
 }
