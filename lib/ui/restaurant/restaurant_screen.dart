b0aebaab253ab5fe6957d9fdd7d5da5b5435b13b
Melksedeque <freelancer@melksedeque.com.br>
2025-04-16 21:32:56 -0300

    Criação de lista de pratos da página do restaurante

--- lib/ui/restaurant/restaurant_screen.dart
+++ lib/ui/restaurant/restaurant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/model/dish.dart';
 import 'package:myapp/model/restaurant.dart';
 
 class RestaurantScreen extends StatelessWidget {
@@ -7,6 +8,34 @@ class RestaurantScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text(restaurant.name)),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 12.0,
+          children: [
+            Image.asset('assets/${restaurant.imagePath}', width: 128.0),
+            Text(
+              "Mais pedidos",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            Column(
+              children: List.generate(restaurant.dishes.length, (index) {
+                Dish dish = restaurant.dishes[index];
+                return ListTile(
+                  leading: Image.asset(
+                    'assets/dishes/default.png',
+                    width: 48.0,
+                  ),
+                  title: Text(dish.name),
+                  subtitle: Text("R\$${dish.price.toStringAsFixed(2)}"),
+                );
+              }),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
