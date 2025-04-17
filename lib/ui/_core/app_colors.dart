b5243beb0f012e7bf6d8981ab7ded90e7782e3ea
Melksedeque <freelancer@melksedeque.com.br>
2025-04-17 15:18:28 -0300

    Correção de cores do projeto segundo o Figma

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const Color backgroundColor = Color(0xFF202123);
-  static const Color mainColor = Color(0xFFFFA559);
   static const Color boxBackgroundColor = Color(0xFF343541);
+
+  static const Color mainColor = Color(0xFFFFA559);
+  static const Color titleColor = Color(0xFFFFE6C7);
+  static const Color textColor = Color(0xFFDBD7DF);
+  
   static const Color textDangerColor = Color(0xFFEA868F);
   static const Color borderDangerColor = Color(0xFF842029);
   static const Color backgroundDangerColor = Color(0xFF2C0B0E);
+  static const Color textWarningColor = Color(0xFFFFDA6A);
+  static const Color borderWarningColor = Color(0xFF997404);
+  static const Color backgroundWarningColor = Color(0xFF332701);
 }
