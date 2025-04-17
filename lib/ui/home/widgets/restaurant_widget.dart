69c0b4ab6fbc1768e94c557580ed2194f8615697
Melksedeque <freelancer@melksedeque.com.br>
2025-04-16 21:10:37 -0300

    Criação de link com InkWell nos botões de Restaurante para a pagina de restaurante

--- lib/ui/home/widgets/restaurant_widget.dart
+++ lib/ui/home/widgets/restaurant_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/model/restaurant.dart';
+import 'package:myapp/ui/restaurant/restaurant_screen.dart';
 
 class RestaurantWidget extends StatelessWidget {
   final Restaurant restaurant;
@@ -7,27 +8,39 @@ class RestaurantWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      spacing: 16.0,
-      children: [
-        Image.asset('assets/${restaurant.imagePath}', width: 72.0),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 4.0,
-          children: [
-            Text(
-              restaurant.name,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
-            ),
-            Row(
-              children: List.generate(restaurant.stars.toInt(), (index) {
-                return Image.asset('assets/others/star.png', width: 16.0);
-              }),
-            ),
-            Text("${restaurant.distance} km"),
-          ],
-        ),
-      ],
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return RestaurantScreen(restaurant: restaurant);
+            },
+          ),
+        );
+      },
+      child: Row(
+        spacing: 16.0,
+        children: [
+          Image.asset('assets/${restaurant.imagePath}', width: 72.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 4.0,
+            children: [
+              Text(
+                restaurant.name,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+              ),
+              Row(
+                children: List.generate(restaurant.stars.toInt(), (index) {
+                  return Image.asset('assets/others/star.png', width: 16.0);
+                }),
+              ),
+              Text("${restaurant.distance} km"),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
