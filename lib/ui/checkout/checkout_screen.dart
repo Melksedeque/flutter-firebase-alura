ffd1542f70de7c612b206487a9fc7578e20efc0f
Melksedeque <freelancer@melksedeque.com.br>
2025-04-17 14:30:52 -0300

    Tela de checkout com os botões de adicionar ou remover item

--- lib/ui/checkout/checkout_screen.dart
+++ lib/ui/checkout/checkout_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/model/dish.dart';
+import 'package:myapp/ui/_core/bag_provider.dart';
+import 'package:provider/provider.dart' show Provider;
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    BagProvider bagProvider = Provider.of<BagProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sacola"),
+        actions: [TextButton(onPressed: () {}, child: Text("Limpar"))],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text("Pedidos"),
+              Column(
+                children: List.generate(
+                  bagProvider.getMapByAmount().keys.length,
+                  (index) {
+                    Dish dish =
+                        bagProvider.getMapByAmount().keys.toList()[index];
+
+                    return ListTile(
+                      leading: Image.asset(
+                        'assets/dishes/default.png',
+                        height: 48.0,
+                        width: 48.0,
+                      ),
+                      title: Text(dish.name),
+                      subtitle: Text("R\$${dish.price.toStringAsFixed(2)}"),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              bagProvider.addAllDishes([dish]);
+                            },
+                            icon: Icon(Icons.add),
+                          ),
+                          Text(
+                            bagProvider.getMapByAmount()[dish].toString(),
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              bagProvider.removeDish(dish);
+                            },
+                            icon: Icon(Icons.remove),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
