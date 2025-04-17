106f537461f51c782be2ebf8a0d674dc33a17415
Melksedeque <freelancer@melksedeque.com.br>
2025-04-17 15:00:25 -0300

    Cores de alert danger do Bootstrap

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -4,4 +4,7 @@ abstract class AppColors {
   static const Color backgroundColor = Color(0xFF202123);
   static const Color mainColor = Color(0xFFFFA559);
   static const Color boxBackgroundColor = Color(0xFF343541);
+  static const Color textDangerColor = Color(0xFFEA868F);
+  static const Color borderDangerColor = Color(0xFF842029);
+  static const Color backgroundDangerColor = Color(0xFF2C0B0E);
 }
