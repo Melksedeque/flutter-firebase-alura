07222a938d8796a817e165351dd487c67bb29a28
Melksedeque <freelancer@melksedeque.com.br>
2025-04-17 15:27:32 -0300

    Ajuste nas cores do projeto segundo o Figma

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -7,7 +7,7 @@ abstract class AppColors {
   static const Color mainColor = Color(0xFFFFA559);
   static const Color titleColor = Color(0xFFFFE6C7);
   static const Color textColor = Color(0xFFDBD7DF);
-  
+
   static const Color textDangerColor = Color(0xFFEA868F);
   static const Color borderDangerColor = Color(0xFF842029);
   static const Color backgroundDangerColor = Color(0xFF2C0B0E);
